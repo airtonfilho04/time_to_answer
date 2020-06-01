2c5422da4f1e938bf5e36d8dff5b184ddeee5032
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-06-01 08:09:50 -0300

    Aula 164

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
-    resources :admins
+    get 'welcome/index' # Dashboard
+    resources :admins # Administradores
+    resources :subjects # Assuntos
   end
   devise_for :admins
 
