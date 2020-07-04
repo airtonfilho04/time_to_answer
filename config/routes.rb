769c9f31b4fb84fb65432dce66f750935a94ffab
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-07-04 14:49:02 -0300

    Aula 217

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :admins
+  devise_for :admins, skip:  [:registrations]
   
   namespace :site do
-    get 'welcome/index'
-    get 'search', to: 'search#questions'
+    get  'welcome/index'
+    get  'search', to: 'search#questions'
+    post 'answer', to: 'answer#question'
+    get  'subject/:subject_id/:subject', to: 'search#subject', as: 'search_subject'
   end
   
   namespace :users_backoffice do
     get 'welcome/index'
+    get 'profile', to: 'profile#edit'
+    patch 'profile', to: 'profile#update'
   end
   
   namespace :admins_backoffice do
@@ -19,6 +23,7 @@ Rails.application.routes.draw do
   end
 
   get 'inicio', to: 'site/welcome#index'
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
 
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
