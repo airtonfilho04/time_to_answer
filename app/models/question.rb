7ed8a4634252537812bb989f8679662f23a49709
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-07-05 00:45:53 -0300

    Aula 232

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,9 @@ class Question < ApplicationRecord
   # Kaminari
   paginates_per 5
 
+  # Callback
+  after_create :set_statistic
+
   # Scopes
   scope :_search_subject_, ->(page, subject_id){
     includes(:answers, :subject)
@@ -22,4 +25,10 @@ class Question < ApplicationRecord
   scope :last_questions, ->(page) {
     includes(:answers, :subject).order('created_at desc').page(page)
   }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+  end
 end
